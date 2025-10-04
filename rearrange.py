47890f64ae09349e1583e837e8d637d9531f4aae
Sinsina V <sinsinav505@gmail.com>
2025-10-04 22:37:36 +0530

    rearrange

--- rearrange.py
+++ rearrange.py
@@ -1,6 +1,6 @@
 #Your task is to make a function that can take any non-negative integer as an argument and 
 # return it with its digits in descending order. 
-# Essentially, rearrange the digits to create the highest possible number.
+# Essentially, rearrange the digits to create the highest possible number..
 
 def descending_order(num):
     l=list(str(num))
